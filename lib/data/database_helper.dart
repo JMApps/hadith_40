7224538cc7cb688192f08f7609cf5a9400a3e4aa
jmapps <jmappsmuslim@gmail.com>
2021-10-06 16:59:51 +0300

    Added data base helper with data base and queries

---
+++ lib/data/database_helper.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper.internal();
+
+  factory DatabaseHelper() => _instance;
+  static Database? _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await initializeDatabase();
+    return _db!;
+  }
+
+  DatabaseHelper.internal();
+
+  Future<Database> initializeDatabase() async {
+    Directory? documentDirectory = Platform.isAndroid
+        ? await getExternalStorageDirectory()
+        : await getApplicationSupportDirectory();
+    String path = join(documentDirectory!.path, 'hadeeth.db');
+
+    var exists = await databaseExists(path);
+
+    if (!exists) {
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {
+        Exception('Invalid database');
+      }
+      ByteData data =
+          await rootBundle.load(join('assets/databases', 'hadeeth.db'));
+      List<int> bytes =
+          data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(path).writeAsBytes(bytes, flush: true);
+    }
+
+    var bomDataTable = await openDatabase(path, version: 1, onUpgrade: _onUpgrade);
+    return bomDataTable;
+  }
+
+  _onUpgrade(Database db, int oldVersion, int newVersion) async {}
+}
