3296d18d9de189fbf3f8ab9e3fbb8b1a14823182
jmapps <jmappsmuslim@gmail.com>
2021-10-07 13:39:23 +0300

    Added hadeeth apart content page

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hadith_40/pages/hadeeth_apart_content.dart';
 import 'package:hadith_40/pages/hadeeth_content.dart';
 import 'package:hadith_40/pages/main_page.dart';
 
@@ -11,6 +12,10 @@ class AppRouter {
       case '/hadeeth_content':
         return MaterialPageRoute(
             builder: (_) => const HadeethContent(), settings: routeSettings);
+      case '/hadeeth_apart_content':
+        return MaterialPageRoute(
+            builder: (_) => const HadeethApartContent(),
+            settings: routeSettings);
       default:
         throw Exception('Invalid route');
     }
