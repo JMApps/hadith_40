054442190b4000069815e96a8453488c1fccdc08
jmapps <jmappsmuslim@gmail.com>
2021-10-12 14:47:06 +0300

    Added close keyboard method

--- lib/pages/main_hadeeth_list.dart
+++ lib/pages/main_hadeeth_list.dart
@@ -14,44 +14,52 @@ class MainHadeethList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        title: const Text(
-          '40 хадисов',
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        backgroundColor: Colors.grey[800],
-      ),
-      body: Column(
-        children: [
-          const HadeethSearcher(),
-          Expanded(
-            child: FutureBuilder<List>(
-              future: context.watch<SearchData>().getTextFieldText.isNotEmpty || context.watch<BookmarkState>().getUpdateList
-                  ? _databaseQuery.getSearchResult(context.watch<SearchData>().getTextFieldText)
-                  : _databaseQuery.getAllHadeeths(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                return snapshot.hasError
-                    ? const Center(
-                        child: Text(
-                          'По вашему запросу ничего не найдено',
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      )
-                    : snapshot.hasData
-                        ? HadeethList(snapshot: snapshot)
-                        : const Center(
-                            child: CupertinoActivityIndicator(),
-                          );
-              },
+    FocusScopeNode _currentKeyBoardFocus = FocusScope.of(context);
+    return GestureDetector(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          title: const Text(
+            '40 хадисов',
+            style: TextStyle(
+              color: Colors.white,
             ),
           ),
-        ],
+          backgroundColor: Colors.grey[800],
+        ),
+        body: Column(
+          children: [
+            const HadeethSearcher(),
+            Expanded(
+              child: FutureBuilder<List>(
+                future: context.watch<SearchData>().getTextFieldText.isNotEmpty || context.watch<BookmarkState>().getUpdateList
+                    ? _databaseQuery.getSearchResult(context.watch<SearchData>().getTextFieldText)
+                    : _databaseQuery.getAllHadeeths(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  return snapshot.hasError
+                      ? const Center(
+                          child: Text(
+                            'По вашему запросу ничего не найдено',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        )
+                      : snapshot.hasData
+                          ? HadeethList(snapshot: snapshot)
+                          : const Center(
+                              child: CupertinoActivityIndicator(),
+                            );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
+      onTap: () {
+        if (!_currentKeyBoardFocus.hasPrimaryFocus) {
+          _currentKeyBoardFocus.unfocus();
+        }
+      },
     );
   }
 }
