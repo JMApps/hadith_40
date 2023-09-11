03bc9478700ab5e1304abc2f9665946e2ec59293
dev_muslim <jmappsmuslim@gmail.com>
2023-09-11 06:46:59 +0300

    Added directories

--- lib/presentation/pages/root_page.dart
+++ lib/presentation/pages/root_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:hadith_40/application/routes/app_routes.dart';
-import 'package:hadith_40/application/themes/app_theme.dart';
+import 'package:hadith_40/core/routes/app_routes.dart';
+import 'package:hadith_40/core/themes/app_theme.dart';
 
 import 'main_page.dart';
 
