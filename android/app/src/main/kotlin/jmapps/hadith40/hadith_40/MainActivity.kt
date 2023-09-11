c7822be4451aeab5e10e49c72a71b99af6470ebc
dev_muslim <jmappsmuslim@gmail.com>
2023-09-11 05:52:07 +0300

    Clear all project

--- android/app/src/main/kotlin/jmapps/hadith40/hadith_40/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package jmapps.hadith40.hadith_40
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,3 @@
-import 'package:flutter/material.dart';
-import 'package:hadith_40/provider/bookmark_state.dart';
-import 'package:hadith_40/provider/hadeeth_settings_state.dart';
-import 'package:hadith_40/provider/main_player_state.dart';
-import 'package:hadith_40/provider/navigator_index_state.dart';
-import 'package:hadith_40/provider/search_data.dart';
-import 'package:hadith_40/router/app_router.dart';
-import 'package:provider/provider.dart';
-
 void main() {
-  final appRouter = AppRouter();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider<NavigatorIndexState>(
-            create: (_) => NavigatorIndexState()),
-        ChangeNotifierProvider<SearchData>(create: (_) => SearchData()),
-        ChangeNotifierProvider<BookmarkState>(create: (_) => BookmarkState()),
-        ChangeNotifierProvider<HadeethSettingsState>(
-            create: (_) => HadeethSettingsState()),
-        ChangeNotifierProvider<MainPlayerState>(
-            create: (_) => MainPlayerState()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: '40 хадисов',
-        theme: ThemeData(
-          primarySwatch: Colors.grey,
-          fontFamily: 'Gilroy',
-        ),
-        onGenerateRoute: appRouter.onGenerateRouter,
-        initialRoute: '/',
-      ),
-    ),
-  );
+
 }
