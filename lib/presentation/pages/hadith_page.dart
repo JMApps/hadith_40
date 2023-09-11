3d4f468e858d9a581039430a842d7ce464cc6c9d
dev_muslim <jmappsmuslim@gmail.com>
2023-09-11 16:29:05 +0300

    Added content to hadith page

--- lib/presentation/pages/hadith_page.dart
+++ lib/presentation/pages/hadith_page.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hadith_40/data/datasources/local/databases/database_helper.dart';
 import 'package:hadith_40/data/repositories/hadith_data_repository.dart';
 import 'package:hadith_40/domain/entities/hadith_entity.dart';
 import 'package:hadith_40/domain/usecases/all_hadith_use_case.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hadith_40/presentation/items/main_hadith_item.dart';
 import 'package:hadith_40/presentation/widgets/error_text.dart';
 
 class HadithPage extends StatefulWidget {
@@ -29,31 +31,33 @@ class _HadithPageState extends State<HadithPage> {
   @override
   Widget build(BuildContext context) {
     final AppLocalizations locale = AppLocalizations.of(context)!;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          locale.appName,
-        ),
-      ),
-      body: FutureBuilder<List<HadithEntity>>(
-        future: _allHadithUseCase.getAllHadiths(tableName: locale.tableName),
-        builder: (BuildContext context, AsyncSnapshot<List<HadithEntity>> snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (BuildContext context, int index) {
-                final HadithEntity hadithEntity = snapshot.data![index];
-                return Text(hadithEntity.hadithTitle);
-              },
-            );
-          } else if (snapshot.hasError) {
-            return ErrorText(errorMessage: snapshot.error.toString());
-          }
-          return const Center(
-            child: CircularProgressIndicator.adaptive(),
+    return FutureBuilder<List<HadithEntity>>(
+      future: _allHadithUseCase.getAllHadiths(tableName: locale.tableName),
+      builder: (BuildContext context, AsyncSnapshot<List<HadithEntity>> snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(locale.appName),
+            ),
+            body: CupertinoScrollbar(
+              child: ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final HadithEntity hadithEntity = snapshot.data![index];
+                  return MainHadithItem(model: hadithEntity, index: index);
+                },
+              ),
+            ),
           );
-        },
-      ),
+        } else if (snapshot.hasError) {
+          return ErrorText(
+            errorMessage: snapshot.error.toString(),
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator.adaptive(),
+        );
+      },
     );
   }
 }
