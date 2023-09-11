3074a9ba17dbb88b4cd82a303975f4ac3b980afe
dev_muslim <jmappsmuslim@gmail.com>
2023-09-11 16:28:23 +0300

    Added hadith item

---
+++ lib/presentation/items/main_hadith_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hadith_40/domain/entities/hadith_entity.dart';
+
+class MainHadithItem extends StatelessWidget {
+  const MainHadithItem({super.key, required this.model, required this.index});
+
+  final HadithEntity model;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme appColors = Theme.of(context).colorScheme;
+    final Color oddItemColor = appColors.primary.withOpacity(0.15);
+    final Color evenItemColor = appColors.primary.withOpacity(0.05);
+    return ListTile(
+      onTap: () {},
+      tileColor: index.isOdd ? oddItemColor : evenItemColor,
+      leading: Icon(
+        CupertinoIcons.bookmark,
+        color: appColors.primary,
+      ),
+      title: Text(
+        model.hadithNumber,
+        style: TextStyle(
+          color: appColors.primary,
+          fontSize: 18,
+        ),
+      ),
+      subtitle: Text(
+        model.hadithTitle,
+        style: const TextStyle(
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+}
