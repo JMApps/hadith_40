f53ce8242152f61e418e603d76d9353834bc5bc5
jmapps <jmappsmuslim@gmail.com>
2024-10-23 19:19:30 +0300

    Update database service

---
+++ lib/core/strings/database_values.dart
@@ -0,0 +1,11 @@
+class DatabaseValues {
+  static const String dbName = 'hadith.db';
+  static const int dbVersion = 1;
+
+  static const String dbId = 'id';
+  static const String dbHadithNumber = 'hadith_number';
+  static const String dbHadithTitle = 'hadith_title';
+  static const String dbHadithArabic = 'hadith_arabic';
+  static const String dbHadithTranslation = 'hadith_translation';
+  static const String dbNameAudio = 'name_audio';
+}
---
+++ lib/data/services/database_service.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+
+import 'package:sqflite/sqflite.dart';
+
+import '../../core/strings/database_values.dart';
+
+class DatabaseService {
+  static Database? _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await initializeDatabase();
+    return _db!;
+  }
+
+  Future<Database> initializeDatabase() async {
+    final databasePath = await getDatabasesPath();
+    String path = join(databasePath, DatabaseValues.dbName);
+
+    var database = await openDatabase(path);
+
+    if (await database.getVersion() < DatabaseValues.dbVersion) {
+      database.close();
+      await deleteDatabase(path);
+
+      try {
+        await Directory(dirname(path)).create(recursive: true);
+      } catch (_) {}
+
+      ByteData data = await rootBundle.load(join('assets/databases', DatabaseValues.dbName));
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      await File(path).writeAsBytes(bytes, flush: true);
+
+      database = await openDatabase(path);
+      database.setVersion(DatabaseValues.dbVersion);
+    }
+
+    return database;
+  }
+}
