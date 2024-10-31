7ad911f7d64d6ca241dc6ab2726820e5697ddd42
jmapps <jmappsmuslim@gmail.com>
2024-10-31 19:54:40 +0300

    Update for android version

--- android/app/src/main/kotlin/com/example/hadith_40/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hadith_40
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/jmapps/hadith_40/MainActivity.kt
@@ -0,0 +1,5 @@
+package jmapps.hadith_40
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
